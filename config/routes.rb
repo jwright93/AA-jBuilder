1f4351d402548b8a275961596d1565d825a49d5e
Jonathon Wright <jonwright93@gmail.com>
2017-03-08 11:41:53 -0800

    Add views for gifts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :guests, only: [:index, :show] do
-      resources :gifts, only: [:index, :show]
+      resources :gifts, only: [:index]
     end
+    resources :gifts, only: [:show]
   end
 end
